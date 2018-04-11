45d0a8ac57940db00fb44f23f289966305962205
Jackson Sui <kiro705@gmail.com>
2018-04-11 16:15:09 -0400

    added deleting

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -3,7 +3,7 @@
 
     angular
     .module('ngClassifieds')
-    .controller('classifiedsCtrl', function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast){
+    .controller('classifiedsCtrl', function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast, $mdDialog){
         classifiedsFactory.getClassifieds()
         .then(function(classifieds){
           $scope.classifieds = classifieds.data
@@ -27,15 +27,46 @@
           if(newClassified){
             newClassified.contact = contact
             $scope.classifieds.unshift(newClassified)
-            $mdSidenav('left').close()
+            $scope.closeSidebar()
             $scope.newClassified = null
-            $mdToast.show(
-              $mdToast.simple()
-                .content('Listing Saved!')
-                .position('top, right')
-                .hideDelay(35000)
-            )
+            showToast('Listing Saved!')
           }
+        }
+
+        $scope.editClassified = function(classifiedForEditing) {
+          $scope.editing = true
+          $scope.openSidebar()
+          $scope.newClassified = classifiedForEditing
+        }
+
+        $scope.saveEdit = function() {
+          $scope.editing = false
+          $scope.newClassified = null
+          $scope.closeSidebar()
+          showToast('Listing Edited!')
+        }
+
+        $scope.deleteClassified = function(event, targetClassified) {
+          const confirm = $mdDialog.confirm()
+            .title('Are you sure you want to delete ' + targetClassified.title + '?')
+            .ok('Yes')
+            .cancel('No')
+            .targetEvent(event) 
+          $mdDialog.show(confirm).then(function(){
+            const index = $scope.classifieds.indexOf(targetClassified)
+            $scope.classifieds.splice(index, 1)
+          }, function() {
+
+          })
+        }
+
+        function showToast(message){
+          $mdToast.show(
+            $mdToast.simple()
+              .content(message)
+              .position('top, right')
+              .hideDelay(35000)
+          )
         }
     })
 })();
